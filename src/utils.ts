c373c48c04260a698ce546feed654d1249e03697
Frederic Arnold <frederic.arnold97@gmail.com>
2024-12-05 23:31:59 +0100

    Set functions, better range function

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,9 @@
-export function range(n: number) {
-    return Array.from({length: n}, (_, i) => i);
+//   Array
+//   =====
+
+export function range(lower: number, upper?: number) {
+    if(upper === undefined) { upper = lower; lower = 0; }
+    return Array.from({length: upper - lower}, (_, i) => i+lower);
 }
 
 export const zip = <T, U>(a: T[], b: U[]): [T, U][] => {
@@ -22,6 +26,40 @@ export const zipWith = <T, U, V>(f: (arg0: T, arg1: U) => V, a: T[], b: U[]): V[
 
 export const count = <T>(f: (arg0: T) => boolean, arr: T[]): number => {
     let acc = 0;
-    arr.forEach((x) => f(x) ? acc++ : 0);
+    for(const x of arr) { if(f(x)) { acc++; } }
     return acc;
+}
+
+//   Set
+//   ===
+
+declare global {
+    interface Set<T> {
+        empty(): boolean;
+    }
+}
+
+Set.prototype.empty = function (): boolean {
+    return this.size === 0;
+}
+
+export const union = <T>(...args: Set<T>[]): Set<T> => {
+    const outputSet: Set<T> = new Set([]);
+    for(const set of args) {
+        for(const x of set) { outputSet.add(x); }
+    }
+    return outputSet;
+}
+
+export const intersection = <T>(set0: Set<T>, ...sets: Set<T>[]): Set<T> => {
+    const outputSet: Set<T> = new Set(set0);
+    // for(const x of set0) { if(set1.has(x)) { outputSet.add(x); } }
+    for(const set of sets) {
+        const itemsToDelete = []
+        for(const elem of outputSet) {
+            if(!set.has(elem)) {itemsToDelete.push(elem)}
+        }
+        for(const elem of itemsToDelete) { outputSet.delete(elem); }
+    }
+    return outputSet;
 }
